5c78339ccaa64988edc53041cb31a10e4b82350e
Dokku <vsdokku@cgifederal.com>
2020-05-23 23:02:54 -0400

    rsocket-practice

---
+++ spring-rsocket-practice/src/main/java/com/learn/springrsocketpractice/SpringRsocketPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springrsocketpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRsocketPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRsocketPracticeApplication.class, args);
+	}
+
+}
---
+++ spring-rsocket-practice/src/test/java/com/learn/springrsocketpractice/SpringRsocketPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springrsocketpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRsocketPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
